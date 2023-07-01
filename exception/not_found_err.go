229a9fde5e823ddd9dd86474167bec6372586d7f
mhd ian rush <mhdianrush@gmail.com>
2023-07-02 06:42:32 +0700

    Update not_found_err.go

--- exception/not_found_err.go
+++ exception/not_found_err.go
@@ -1,11 +1,9 @@
 package exception
 
 type NotFoundError struct {
-	// disini akan mengikuti kontrak interface error
 	Error string
 }
 
 func NewNotFoundError(error string) NotFoundError {
-	// nah disini kita menggunakan return value nya adalah struct NotFoundError
 	return NotFoundError{Error: error}
 }
