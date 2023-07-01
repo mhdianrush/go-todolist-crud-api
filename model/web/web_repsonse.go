9a34f2a786a134185f565d91b4dc3f7c8f4f737c
mhd ian rush <mhdianrush@gmail.com>
2023-07-02 06:48:11 +0700

    Update web_repsonse.go

--- model/web/web_repsonse.go
+++ model/web/web_repsonse.go
@@ -4,6 +4,4 @@ type WebResponse struct {
 	Code   int         `json:"code"`
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
-	// Code, Status, dan Data disini adalah representasi dari data JSON yang sebelumnya
-	// Data menggunakan interface{} karena kan di dalam Data pada JSON tersebut terdapat id dan name
 }
