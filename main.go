f9a8caff0dd91f868e2c17e8dbb7d976a3ead4a1
mhd ian rush <mhdianrush@gmail.com>
2023-07-02 07:31:34 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 
 
 func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
-	// menggunakan *middleware.AuthMiddleware karena AuthMiddleware adalah sebuah struct di folder middleware
 	return &http.Server{
 		Addr: "localhost:3000",
 		Handler: authMiddleware,
